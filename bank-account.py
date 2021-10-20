b8cca05292e69b00217885b65710a7e91568d551
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-10-20 14:55:07 -0700

    added deposit method

--- bank-account.py
+++ bank-account.py
@@ -3,4 +3,10 @@ class BankAccount:
         self.name = full_name
         self.accountNum = account_number
         self.balance = balance
+    
+    def deposit(self,amount):
+        self.balance += amount
+        print(f'Amount deposited: ${amount} new balance: ${self.balance}')
         
+test = BankAccount("jenn", "0123", 30)
+test.deposit(30)
