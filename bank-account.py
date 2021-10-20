45976068917a22c98424277ea7b4bd199c02b0a8
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-10-20 14:43:36 -0700

    added class

---
+++ bank-account.py
@@ -0,0 +1,6 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, balance):
+        self.name = full_name
+        self.accountNum = account_number
+        self.balance = balance
+        
